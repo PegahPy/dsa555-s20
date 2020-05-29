12c5df72991954a07f171adddf49bcde9fd8f97d
Catherine Leung <catherine.leung>
2020-05-29 00:49:52 -0400

    lab and a1 starter/tester file backup

---
+++ a1files/a1q2.cpp
@@ -0,0 +1,9 @@
+bool candidate(int array[],int& candidate, int sz);
+bool majorityElement(const int arr[], int& majority, int sz);
+
+bool majorityElement(const int arr[], int& majority, int sz){
+	
+}
+bool candidate(int array[],int& candidate, int sz){
+	
+}
---
+++ a1files/sortedlist.h
@@ -0,0 +1,111 @@
+
+template <typename T>
+
+class SortedList{
+	struct Node{
+
+		Node(const T& data=T{},Node* nx=nullptr,Node* pr=nullptr){
+			
+		}
+	};
+
+public:
+	class const_iterator{
+	public:
+		const_iterator(){}
+		const_iterator& operator++(){}
+		const_iterator operator++(int){}
+		const_iterator& operator--(){}
+		const_iterator operator--(int){}
+		bool operator==(const_iterator rhs){}
+		bool operator!=(const_iterator rhs){}
+		const T& operator*()const{}
+	};
+	class iterator:public const_iterator{
+	public:
+		iterator(){}
+		iterator& operator++(){}
+		iterator operator++(int){}
+		iterator& operator--(){}
+		iterator operator--(int){}
+		T& operator*(){}
+		const T& operator*()const{}
+	};
+	SortedList();
+	~SortedList();
+	SortedList(const SortedList& rhs);
+	SortedList& operator=(const SortedList& rhs);
+	SortedList(SortedList&& rhs);
+	SortedList& operator=(SortedList&& rhs);
+	iterator begin(){}
+	iterator end(){}
+	const_iterator cbegin() const{}
+	const_iterator cend() const{}
+	iterator insert(const T& data);
+	iterator search(const T& data);
+	const_iterator search(const T& data) const;
+	iterator erase(iterator it);
+	iterator erase(iterator first, iterator last);
+	bool empty() const;
+	int size() const;
+};
+
+template <typename T>
+SortedList<T>::SortedList(){
+
+}
+template <typename T>
+SortedList<T>::~SortedList(){
+
+}
+template <typename T>
+SortedList<T>::SortedList(const SortedList& rhs){
+
+}
+template <typename T>
+SortedList<T>& SortedList<T>::operator=(const SortedList& rhs){
+
+}
+template <typename T>
+SortedList<T>::SortedList(SortedList&& rhs){
+
+}
+
+template <typename T>
+SortedList<T>& SortedList<T>::operator=(SortedList&& rhs){
+
+}
+
+
+template <typename T>
+typename SortedList<T>::iterator SortedList<T>::insert(const T& data){
+
+}
+
+template <typename T>
+typename SortedList<T>::iterator SortedList<T>::search(const T& data){
+
+}
+
+template <typename T>
+typename SortedList<T>::const_iterator SortedList<T>::search(const T& data) const{
+
+}
+
+template <typename T>
+typename SortedList<T>::iterator SortedList<T>::erase(iterator it){
+
+}
+
+template <typename T>
+typename SortedList<T>::iterator SortedList<T>::erase(iterator first, iterator last){
+
+}
+template <typename T>
+bool SortedList<T>::empty() const{
+
+}
+template <typename T>
+int SortedList<T>::size() const{
+
+}
