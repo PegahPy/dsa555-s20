c33709f36e5f787f84aaf3a0444281836f09f469
catherine-leung <1699186+catherine-leung@users.noreply.github.com>
2020-05-30 16:53:31 -0400

    Fixed includes
    Fixed includes to correctly use starter file name and also added include for cstring

--- a1files/a1q1tester.cpp
+++ a1files/a1q1tester.cpp
@@ -5,7 +5,8 @@
 /*   To compile: g++ a1q1tester.cpp timer.cpp -std=c++0x                  */
 /*                                                                        */
 /*                                                                        */
-/*   version: 1.0                                                         */
+/*   version: 1.1: Fixed include for correct sortedlist.h, added          */
+/*                 include for cstring                                    */
 /*                                                                        */
 /**************************************************************************/
 
@@ -14,7 +15,7 @@
 #include <string>
 #include <iostream>
 #include <cstdlib>
-
+#include <cstring>
 
 bool test1(std::string& error); 
 bool test2(std::string& error); 
