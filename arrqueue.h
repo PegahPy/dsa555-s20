440b82bf721c73e99777fee362f3dcada7c37b68
Catherine Leung <catherine.leung>
2020-06-19 16:32:03 -0400

    fixed queue

--- arrqueue.h
+++ arrqueue.h
@@ -5,8 +5,21 @@ class Queue{
 	int used_;
 	int front_;
 	int back_;
+	void grow(){
+		T* temp = new T[capacity_*2];
+		int j=front_;
+		for(int i=0;i<used_;i++){
+			temp[i]=theQueue_[j];
+			j=(j+1)%capacity_;
+		}
+		delete [] theQueue_;
+		theQueue_= temp;
+		capacity_=capacity_*2;	
+		front_=0;
+		back_=used_;
+	}
 public:
-	Queue(int capacity=100);{
+	Queue(int capacity=100){
 		theQueue_=new T[capacity];
 		capacity_=capacity;
 		used_=0;
@@ -14,7 +27,7 @@ public:
 		back_=0;
 	}
 	void enqueue(const T& data){
-		if(used_ == capcity){
+		if(used_ == capacity_){
 			grow();
 		}
 		theQueue_[back_]=data;
